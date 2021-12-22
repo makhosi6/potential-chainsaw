be6ec606ada25b9ddf4de5a5cf6ff716889c7dde
makhosi6 <ndondo330@gmail.com>
2021-12-23 01:43:58 +0200

    Thursday 12/23/2021 01:43 - Error Handling, Routes, Comments, middleware

--- app.js
+++ app.js
@@ -1,32 +1,79 @@
+const puppeteer = require("puppeteer");
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 2020;
-const {minimal_args} = require("./helpers/browser")
-const {
-    task
-} = require("./jobs/search");
-const puppeteer = require("puppeteer");
+const _search = express.Router();
+const _error = express.Router();
 /**
- *
+ * 
+ */
+const {
+    minimal_args
+} = require("./helpers/browser")
+const {
+    task,
+    log
+} = require("./jobs/search");
+const PORT = process.env.PORT || 2020;
+
+app.use(function (req, res, next) {
+    console.log({
+        METHOD: req.method
+    })
+    if (req.method === 'GET') {
+        next()
+    } else {
+        res.status(405).json({
+            status: 405,
+            message: "Method Not Allowed",
+            path: req.originalUrl
+        })
+    }
+
+})
+
+/**
+ * Launch the browser 
+ * @tutorial {@link Puppeteer} https://pptr.dev/#?product=Puppeteer&version=v13.0.1&show=api-class-browser
  *
  */
-
 const launchTheBrowser = async () => await puppeteer.launch({
     headless: false,
     args: minimal_args,
     userDataDir: './data/user_dir'
 })
 Promise.resolve(launchTheBrowser()).then(function (browser) {
-    console.log({
-        pptrWsURL: browser.wsEndpoint()
-    });
-    app.get("/api/v1/:search", async function (req, res) {
+    _search.get("/:search", async function (req, res) {
         let search = req.query.search || req.params.search
-        let data = await task(browser, search );
-        res.send({
-            data
-        });
+        let url = req.protocol + '://' + req.get('host') + req.originalUrl;
+        console.log("\x1b[43m%s\x1b[0m", url)
+        task(browser, search).then(data => {
+            // if (JSON.stringify({}) === JSON.stringify(data)) {
+            if (log.length === 0) {
+                res.send({
+                    status: 500,
+                    message: "Internal Server Error"
+                })
+            } else {
+                res.send({
+                    status: 200,
+                    path: req.originalUrl,
+                    "total": log.length,
+                    current_page: 1,
+                    next_page: null,
+                    data
+                });
+            }
+        })
     });
-    app.listen(port, () => console.log(`listening on http://localhost:${port}`));
+    _error.get('*', function (req, res) {
+        res.status(404).json({
+            status: 404,
+            message: "Resource Not Found",
+            path: req.originalUrl
+        })
+    });
+
+    app.use('/api/v1/', [_search, _error]);
+    app.listen(PORT, () => console.log("\x1b[42m%s\x1b[0m", `listening on http://localhost:${PORT}`));
 
 })
--- jobs/search.js
+++ jobs/search.js
@@ -1,11 +1,16 @@
 // const browser = require("../helpers/browser");
+/**
+ * Keep track of properties of the data object 
+ * AND Monitor and keep track of error(s), So we can exit the loops
+ */
+ let log = []
 
 /**
  * 'var' statement declares a function-scoped or globally-scoped variable, so you will see 'var' in a number of place. DO PANIC!!
- * 
- * 
+ * @param {puppeteer.Browser} browser 
+ * @param {string} search 
+ * @returns {object} data
  */
-
 const task = async (browser, search) => {
   let data = {}
   try {
@@ -14,29 +19,34 @@ const task = async (browser, search) => {
     var page = await browser.newPage();
     await page.setRequestInterception(true);
     page.on("request", (req) => {
-      if (
-        req.resourceType() == "stylesheet" ||
-        req.resourceType() == "font" ||
-        req.resourceType() == "image" ||
-        req.resourceType() == "script" ||
-        req.resourceType() == "javascript"
-      ) {
+      if (req.resourceType() == "stylesheet" || req.resourceType() == "font" || req.resourceType() == "image" /** || req.resourceType() == "script"*/ || req.resourceType() == "javascript") {
         req.abort();
       } else {
         req.continue();
       }
     });
-    await page.goto(`https://en.wiktionary.org/wiki/${search}`
+    // await page.goto(`https://en.wiktionary.org/wiki/${search}`
+    await page.goto(`https://en.wiktionary.org/w/index.php?title=${search}`
       /** , { waitUntil: "networkidle2", }*/
     );
     await page.waitForSelector(".mw-headline");
     var hasLoaded = new Date()
     const allHeads = await page.$$("h3");
     const heads = Array.from(allHeads); //.filter((item) => item.id === "")
-    let log = [];
+    /**
+     * Monitor and keep track of error(s), So we can exit the loops
+     * 
+     */
     let err = undefined
+    /**
+     * Monitor and keep track of the index(s), So we can exit the loops
+     */
     let lastIndex;
-
+    /**
+     * Filter junk/bad characters in the provided string
+     * @param {string} param 
+     * @returns {string}
+     */
     function excludeJunkChars(param) {
       let str = param;
       let junk = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "IPA(key):", '.', "quotations", "[", "]"];
@@ -44,7 +54,7 @@ const task = async (browser, search) => {
       junk.map((j) => (j !== last) && (str = str.replace(j, "")));
       return str.replace(/(&nbsp;)*/g, "");
     }
-    let num = 1;
+
     for (let index = 0; index < heads.length; index++) {
       console.log(index);
       try {
@@ -70,24 +80,16 @@ const task = async (browser, search) => {
             definations.push(excludeJunkChars(txt))
           }
         }
-        let subtext =
-          name === "pronunciation" ?
+        let subtext = name === "pronunciation" ?
           await page.evaluate((el) => el.innerText, await page.$(".IPA")) :
           await page.evaluate((el) => el.innerText, sub);
-        let exclude =
-          name.includes("further_reading") ||
-          name.includes("see_also") ||
-          name.includes("anagrams") ||
-          name.includes("references");
-        ///////
-        /**
-         * 
-         */
+        let exclude = name.includes("further_reading") || name.includes("see_also") || name.includes("anagrams") || name.includes("references");
+
         if (lastIndex === log.indexOf(headtext) === -1) break
         lastIndex = (log.indexOf(headtext) === -1) ? true : false;
         if (!exclude && log.indexOf(headtext) === -1) {
           log.push(headtext.trim());
-
+        
           data = {
             ...data,
             [name]: {
@@ -115,7 +117,6 @@ const task = async (browser, search) => {
   } finally {
     page.close()
     var end = new Date()
-
     console.log({
       start,
       end,
@@ -127,5 +128,5 @@ const task = async (browser, search) => {
   ////
 }
 module.exports = {
-  task
+  task, log
 }
